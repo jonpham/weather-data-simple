a3305d8ed1f73e2832901eb0698f17be14ea25e6
Jonathan Pham <jon.pham@gmail.com>
2016-12-07 18:37:58 -0800

    Add Ability to get new City Data.

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,10 +1,26 @@
 class CityController < ApplicationController
+  
   def index
-    if @location.empty?
+    if @location == nil
       @location = {city:'nome',state: 'ak'}
     end
-    url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{city}%2C%20#{state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    @data = Unirest.get(url).body['query']['results']['channel']
+    @data = get_weather_data(@location)
     # data['location']['city']
+    @forecast = @data['item']['forecast']
   end
+
+  def create
+    @location = {city: params[:input_city], state: params[:input_state]}
+    @data = get_weather_data(@location)
+    # data['location']['city']
+    @forecast = @data['item']['forecast']
+    render :index
+  end
+
+  def get_weather_data(location_hash)
+    url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{location_hash[:city]}%2C%20#{location_hash[:state]}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    return Unirest.get(url).body['query']['results']['channel']
+  end
+
+  
 end
